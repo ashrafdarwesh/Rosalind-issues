2afe593dc1476c940b7d5d03b13cf35d77520a1f
ashrafdarwesh <160834350+ashrafdarwesh@users.noreply.github.com>
2024-02-28 14:12:34 +0300

    Update Rabbits-and-Recurrence-Relations.py

--- Rabbits-and-Recurrence-Relations.py
+++ Rabbits-and-Recurrence-Relations.py
@@ -5,7 +5,7 @@ Given: Positive integers n≤40 and k≤5.
 Return: The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs (instead of only 1 pair).
 
 # open the .txt file and extract data from it.
-with open("/Users/HASAN/Desktop/Rosalind/rosalind_fib_1_dataset.txt") as Data_file:
+with open("/Users/ASHRAF/Desktop/Rosalind/rosalind_fib_1_dataset.txt") as Data_file:
     Data = list(map(int, Data_file.readline().strip().split(" ")))
 n = int(Data[0])  # n is the number of months
 k = int(Data[1])  # k is the number of rabbit pairs produced by each rabbit pair
